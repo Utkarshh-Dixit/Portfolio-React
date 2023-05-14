7cda5cbe0da2a2b332851d73801c1bdad53e8955
Utkarsh Dixit <98089242+Utkarshh-Dixit@users.noreply.github.com>
2023-05-15 02:36:26 +0530

    wow

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll} from "framer-motion";
 
 const About = () => {
   const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
+  
   return (
     <>
       <motion.div
